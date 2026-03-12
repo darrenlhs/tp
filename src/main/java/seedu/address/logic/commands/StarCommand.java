95f64b3a883aebc95b23af8223b6981a538804c5
skylar <yujiawang135312@gmail.com>
2026-03-12 17:50:24 +0800

    Add StarCommand and UnstarCommand

---
+++ src/main/java/seedu/address/logic/commands/StarCommand.java
@@ -0,0 +1,85 @@
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.commons.util.ToStringBuilder;
+import seedu.address.logic.Messages;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.person.Person;
+import seedu.address.model.tag.Tag;
+
+/**
+ * Stars a person identified using it's displayed index from the address book.
+ * This is basically the same as adding a tag with the reserved 'STAR' keyword
+ */
+public class StarCommand extends Command {
+
+    public static final String COMMAND_WORD = "star";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Stars / Favourites the person identified by the index number used in the displayed person list.\n"
+            + "Parameters: INDEX (must be a positive integer)\n"
+            + "Example: " + COMMAND_WORD + " 1";
+
+    public static final String MESSAGE_STAR_PERSON_SUCCESS = "Starred Person: %1$s";
+
+    private final Index targetIndex;
+
+    public StarCommand(Index targetIndex) {
+        this.targetIndex = targetIndex;
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+        List<Person> lastShownList = model.getFilteredPersonList();
+
+        if (targetIndex.getZeroBased() >= lastShownList.size()) {
+            throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+        }
+
+        Person personToStar = lastShownList.get(targetIndex.getZeroBased());
+
+        Set<Tag> newTags = new HashSet<>(personToStar.getTags());
+        newTags.add(new Tag(Tag.STAR_TAG));
+
+        Person starredPerson = new Person(
+            personToStar.getName(),
+            personToStar.getPhone(),
+            personToStar.getEmail(),
+            newTags
+        );
+
+        model.setPerson(personToStar, starredPerson);
+
+        return new CommandResult(String.format(MESSAGE_STAR_PERSON_SUCCESS, Messages.format(starredPerson)));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof StarCommand)) {
+            return false;
+        }
+
+        StarCommand otherStarCommand = (StarCommand) other;
+        return targetIndex.equals(otherStarCommand.targetIndex);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .add("targetIndex", targetIndex)
+                .toString();
+    }
+}
--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -18,6 +18,8 @@ import seedu.address.logic.commands.ExitCommand;
 import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.commands.HelpCommand;
 import seedu.address.logic.commands.ListCommand;
+import seedu.address.logic.commands.StarCommand;
+import seedu.address.logic.commands.UnstarCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
 /**
@@ -71,6 +73,12 @@ public class AddressBookParser {
 
         case FindCommand.COMMAND_WORD:
             return new FindCommandParser().parse(arguments);
+
+        case StarCommand.COMMAND_WORD:
+            return new StarCommandParser().parse(arguments);
+
+        case UnstarCommand.COMMAND_WORD:
+            return new UnstarCommandParser().parse(arguments);
 
         case ListCommand.COMMAND_WORD:
             return new ListCommand();
