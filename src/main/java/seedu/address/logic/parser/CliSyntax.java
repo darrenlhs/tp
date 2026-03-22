735ea03efe423205586e249f9433b25f00ae55b2
Raylong268 <shanetanjun@gmail.com>
2026-03-22 13:28:09 +0800

    Remove i/ prefix from AddMeetingParser

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -12,7 +12,6 @@ public class CliSyntax {
     public static final Prefix PREFIX_TAG = new Prefix("t/");
     public static final Prefix PREFIX_SEPARATOR = new Prefix("/");
     public static final Prefix PREFIX_COMMA = new Prefix(",");
-    public static final Prefix PREFIX_MEETING_INDEX = new Prefix("i/");
     public static final Prefix PREFIX_MEETING_DESCRIPTION = new Prefix("d/");
     public static final Prefix PREFIX_MEETING_DATE = new Prefix("dt/");
 }
